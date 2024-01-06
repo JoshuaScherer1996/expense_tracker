a143b8a20751e295d1afefa86586774edbe2ef65
Joshua Scherer <62510842+JoshuaScherer1996@users.noreply.github.com>
2024-01-06 19:11:50 +0100

    created dummy data

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -2,16 +2,20 @@ import 'package:uuid/uuid.dart';
 
 const uuid = Uuid();
 
+enum Category { food, travel, leisure, work }
+
 class Expense {
   // After the expense list there is a initializer list for values used with ':'
   Expense({
     required this.title,
     required this.amount,
     required this.date,
+    required this.category,
   }) : id = uuid.v4();
 
   final String id;
   final String title;
   final double amount;
   final DateTime date;
+  final Category category;
 }
