d2e6df187ec60220f21e6621fa80193c4da5136f
Joshua Scherer <62510842+JoshuaScherer1996@users.noreply.github.com>
2024-01-09 17:46:51 +0100

    added another controller and textfield as an exercise.

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -11,10 +11,12 @@ class NewExpense extends StatefulWidget {
 
 class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
 
   @override
   void dispose() {
     _titleController.dispose();
+    _amountController.dispose();
     super.dispose();
   }
 
@@ -31,18 +33,28 @@ class _NewExpenseState extends State<NewExpense> {
               label: Text('Title'),
             ),
           ),
+          TextField(
+            keyboardType: TextInputType.number,
+            controller: _amountController,
+            maxLength: 20,
+            decoration: const InputDecoration(
+              label: Text('Amount'),
+            ),
+          ),
           Row(
             children: [
               ElevatedButton(
                 onPressed: () {
                   print(_titleController.text);
+                  print(_amountController.text);
                 },
                 child: const Text('save expense'),
               ),
             ],
-          )
+          ),
         ],
       ),
     );
   }
 }
+//ToDo: Add textfield to Column in sperate Row
