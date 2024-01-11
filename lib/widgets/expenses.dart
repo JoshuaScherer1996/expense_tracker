683986ef5c81228cb01681818001b685dd4af30f
Joshua Scherer <62510842+JoshuaScherer1996@users.noreply.github.com>
2024-01-11 17:17:19 +0100

    Displayed a message if our _registeredExpenses is empty.

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -50,6 +50,17 @@ class _ExpensesState extends State<Expenses> {
 
   @override
   Widget build(BuildContext context) {
+    Widget mainContent = const Center(
+      child: Text('No expenss found. Start adding some!'),
+    );
+
+    if (_registeredExpenses.isNotEmpty) {
+      mainContent = ExpensesList(
+        expenses: _registeredExpenses,
+        onRemoveExpense: _removeExpense,
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter Expense Tracker'),
@@ -64,10 +75,7 @@ class _ExpensesState extends State<Expenses> {
         children: [
           const Text('The chart placeholder'),
           Expanded(
-            child: ExpensesList(
-              expenses: _registeredExpenses,
-              onRemoveExpense: _removeExpense,
-            ),
+            child: mainContent,
           )
         ],
       ),
