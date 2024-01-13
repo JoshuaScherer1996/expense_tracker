a16522fac51a8384094eea5b0c7258419a561c75
Joshua Scherer <62510842+JoshuaScherer1996@users.noreply.github.com>
2024-01-13 18:34:39 +0100

    locked the orientation for the non adaptive, basic version of the app.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 // Importing necessary Flutter material package and local widgets.
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/widgets/expenses.dart';
+import 'package:flutter/services.dart';
 
 // Defining the light color scheme for the application.
 var kColorScheme = ColorScheme.fromSeed(
@@ -15,60 +16,69 @@ var kDarkColorScheme = ColorScheme.fromSeed(
 
 // Main entry point of the application.
 void main() {
-  runApp(
-    MaterialApp(
-      // Setting up the dark theme data with specific color schemes and styles.
-      darkTheme: ThemeData.dark().copyWith(
-        useMaterial3: true,
-        colorScheme: kDarkColorScheme,
-        cardTheme: const CardTheme().copyWith(
-          color: kDarkColorScheme.secondaryContainer,
-          margin: const EdgeInsets.symmetric(
-            horizontal: 16,
-            vertical: 8,
-          ),
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: kDarkColorScheme.primaryContainer,
-            foregroundColor: kDarkColorScheme.onPrimaryContainer,
-          ),
-        ),
-      ),
-      // Setting up the light theme data with specific color schemes and styles.
-      theme: ThemeData().copyWith(
-        useMaterial3: true,
-        colorScheme: kColorScheme,
-        appBarTheme: const AppBarTheme().copyWith(
-          backgroundColor: kColorScheme.onPrimaryContainer,
-          foregroundColor: kColorScheme.primaryContainer,
-        ),
-        cardTheme: const CardTheme().copyWith(
-          color: kColorScheme.secondaryContainer,
-          margin: const EdgeInsets.symmetric(
-            horizontal: 16,
-            vertical: 8,
-          ),
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: kColorScheme.primaryContainer,
-          ),
-        ),
-        // Customizing the text theme, particularly the titleLarge style.
-        textTheme: ThemeData().textTheme.copyWith(
-              titleLarge: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: kColorScheme.onSecondaryContainer,
-                fontSize: 16,
+  // Ensures that first locking the orientation and then running the app works.
+  WidgetsFlutterBinding.ensureInitialized();
+  // Forces the app to only run in the given orientation.
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]).then(
+    (fn) {
+      runApp(
+        MaterialApp(
+          // Setting up the dark theme data with specific color schemes and styles.
+          darkTheme: ThemeData.dark().copyWith(
+            useMaterial3: true,
+            colorScheme: kDarkColorScheme,
+            cardTheme: const CardTheme().copyWith(
+              color: kDarkColorScheme.secondaryContainer,
+              margin: const EdgeInsets.symmetric(
+                horizontal: 16,
+                vertical: 8,
               ),
             ),
-      ),
-      // themeMode: ThemeMode.system, // this line is the default
-      // Setting the root widget of the application.
-      home: const Expenses(),
-      // Disabling the debug banner in the upper right corner.
-      debugShowCheckedModeBanner: false,
-    ),
+            elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: kDarkColorScheme.primaryContainer,
+                foregroundColor: kDarkColorScheme.onPrimaryContainer,
+              ),
+            ),
+          ),
+          // Setting up the light theme data with specific color schemes and styles.
+          theme: ThemeData().copyWith(
+            useMaterial3: true,
+            colorScheme: kColorScheme,
+            appBarTheme: const AppBarTheme().copyWith(
+              backgroundColor: kColorScheme.onPrimaryContainer,
+              foregroundColor: kColorScheme.primaryContainer,
+            ),
+            cardTheme: const CardTheme().copyWith(
+              color: kColorScheme.secondaryContainer,
+              margin: const EdgeInsets.symmetric(
+                horizontal: 16,
+                vertical: 8,
+              ),
+            ),
+            elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: kColorScheme.primaryContainer,
+              ),
+            ),
+            // Customizing the text theme, particularly the titleLarge style.
+            textTheme: ThemeData().textTheme.copyWith(
+                  titleLarge: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: kColorScheme.onSecondaryContainer,
+                    fontSize: 16,
+                  ),
+                ),
+          ),
+          // themeMode: ThemeMode.system, // this line is the default
+          // Setting the root widget of the application.
+          home: const Expenses(),
+          // Disabling the debug banner in the upper right corner.
+          debugShowCheckedModeBanner: false,
+        ),
+      );
+    },
   );
 }
